d46f5d94dd37398f713b711b6cf2ee9f91b4b574
Timo <timo23075@gmail.com>
2025-07-19 21:37:48 +0200

    added vfx page

--- js/vfx-script.js
+++ js/vfx-script.js
@@ -1,8 +1,6 @@
 const themeToggleBtn = document.getElementById('theme-toggle')
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon')
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon')
-const star = document.getElementById('star');
-const flower = document.getElementById('flower');
 
 if (
    localStorage.getItem('color-theme') === 'dark' ||
@@ -11,50 +9,34 @@ if (
 ) {
    //show light icon
    themeToggleLightIcon.classList.remove('hidden')
-   star.classList.remove('hidden')
 } else {
    themeToggleDarkIcon.classList.remove('hidden')
-   flower.classList.remove('hidden')
 }
 
 function themeChange() {
    themeToggleDarkIcon.classList.toggle('hidden')
    themeToggleLightIcon.classList.toggle('hidden')
-   console.log('clicked')
+
    //If is set in localstorage
    if (localStorage.getItem('color-theme')) {
       if (localStorage.getItem('color-theme') === 'light'){
          document.documentElement.classList.add('dark')
          localStorage.setItem('color-theme', 'dark')
-         star.classList.remove('hidden')
-         star.classList.add('inline-block')
-         flower.classList.add('hidden')
-         flower.classList.remove('inline-block')
 
       } else {
          document.documentElement.classList.remove('dark')
          localStorage.setItem('color-theme', 'light')
-         star.classList.add('hidden')
-         star.classList.remove('inline-block')
-         flower.classList.remove('hidden')
-         flower.classList.add('inline-block')
          
       }
    } else {
       if (document.documentElement.classList.contains('dark')){
          document.documentElement.classList.remove('dark')
          localStorage.setItem('color-theme', 'light')
-         star.classList.add('hidden')
-         star.classList.remove('inline-block')
-         flower.classList.remove('hidden')
-         flower.classList.add('inline-block')
+
       } else {
          document.documentElement.classList.add('dark')
          localStorage.setItem('color-theme', 'dark')
-         star.classList.remove('hidden')
-         star.classList.add('inline-block')
-         flower.classList.add('hidden')
-         flower.classList.remove('inline-block')
+
       }
    }
 }
